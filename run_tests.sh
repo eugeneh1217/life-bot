f68e9df749aa707af6ceeea7985cf6e6b6581deb
Eugene Hong <eugeneh1217@gmail.com>
2021-05-08 12:27:41 -0400

    removed run_tests.sh:

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# configure virtual environment
-source .venv/bin/activate && pip3 install -r requirements.txt
-
-# run coverage tests
-coverage run -m unittest discover tests && coverage report
